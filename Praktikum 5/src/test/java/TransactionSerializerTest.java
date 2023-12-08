2d8d218c50b2564a62e72144e4b174493d43178f
Skill3rboy <fabian@juelich-computer.de>
2023-12-08 13:32:17 +0100

    god bless america

--- Praktikum 5/src/test/java/TransactionSerializerTest.java
+++ Praktikum 5/src/test/java/TransactionSerializerTest.java
@@ -1,22 +1,31 @@
 import com.google.gson.*;
 import bank.*;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import java.io.File;
 import java.nio.file.*;
 import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class TransactionSerializerTest {
     static String directory = "./test_data";
-    @AfterAll
-    public static void cleanUp(){
+
+    @BeforeEach
+    public void startUp() {
         File Filedirectory = new File(directory);
-        if(Filedirectory.exists()){
+        if (!(Filedirectory.exists())) {
+            Filedirectory.mkdir();
+        }
+    }
+
+    @AfterEach
+    public void cleanUp() {
+        File Filedirectory = new File(directory);
+        if (Filedirectory.exists()) {
             File[] files = Filedirectory.listFiles();
-            if(files!=null){
-                for(File file : files){
+            if (files != null) {
+                for (File file : files) {
                     file.delete();
                 }
             }
@@ -25,17 +34,17 @@ public class TransactionSerializerTest {
     }
 
     @Test
-    public void paymentSerializationTest(){
-        PrivateBank privateBank = new PrivateBank("Private bank",0.5,0.5,directory);
+    public void paymentSerializationTest() {
+        PrivateBank privateBank = new PrivateBank("Private bank", 0.5, 0.5, directory);
 
-        assertDoesNotThrow( ()-> privateBank.createAccount("payment"));
-        Transaction transaction = new Payment("26.11.2023",1000,"Payment",0.5,0.5);
-        assertDoesNotThrow(()->privateBank.addTransaction("payment",transaction));
+        assertDoesNotThrow(() -> privateBank.createAccount("payment"));
+        Transaction transaction = new Payment("26.11.2023", 1000, "Payment", 0.5, 0.5);
+        assertDoesNotThrow(() -> privateBank.addTransaction("payment", transaction));
         List<Transaction> transactionList = privateBank.getAccountsToTransactions().get("payment");
-        assertEquals(1,transactionList.size());
+        assertEquals(1, transactionList.size());
         Transaction transaction1 = transactionList.get(0);
         GsonBuilder gsonBuilder = new GsonBuilder();
-        gsonBuilder.registerTypeAdapter(Payment.class,new TransactionSerializer());
+        gsonBuilder.registerTypeAdapter(Payment.class, new TransactionSerializer());
         Gson gson = gsonBuilder.create();
 
         String json = gson.toJson(transaction1);
@@ -48,25 +57,25 @@ public class TransactionSerializerTest {
                 "\"amount\":1000.0," +
                 "\"description\":\"Payment\"" +
                 "}" + "}";
-        assertEquals(comparison,json);
-        JsonObject jsonObject = gson.fromJson(json,JsonObject.class);
+        assertEquals(comparison, json);
+        JsonObject jsonObject = gson.fromJson(json, JsonObject.class);
         JsonElement classNameElement = jsonObject.get("CLASSNAME");
-        assertNotNull(classNameElement,"CLASSNAME missing");
+        assertNotNull(classNameElement, "CLASSNAME missing");
         assertEquals("Payment", classNameElement.getAsString());
     }
 
     @Test
-    public void paymentDeserializationTest(){
-        PrivateBank privateBank = new PrivateBank("Private Bank", 0.5,0.5,directory);
-        assertDoesNotThrow(()->privateBank.createAccount("payment"));
-        Payment transaction = new Payment("26.11.2023",1000,"Payment",0.5,0.5);
-        assertDoesNotThrow(()->privateBank.addTransaction("payment",transaction));
+    public void paymentDeserializationTest() {
+        PrivateBank privateBank = new PrivateBank("Private Bank", 0.5, 0.5, directory);
+        assertDoesNotThrow(() -> privateBank.createAccount("payment"));
+        Payment transaction = new Payment("26.11.2023", 1000, "Payment", 0.5, 0.5);
+        assertDoesNotThrow(() -> privateBank.addTransaction("payment", transaction));
         List<Transaction> transactionList = privateBank.getAccountsToTransactions().get("payment");
-        assertEquals(1,transactionList.size());
+        assertEquals(1, transactionList.size());
         Transaction transaction1 = transactionList.get(0);
         GsonBuilder gsonBuilder = new GsonBuilder();
-        gsonBuilder.registerTypeAdapter(Payment.class,new TransactionSerializer());
-        Gson gson=gsonBuilder.create();
+        gsonBuilder.registerTypeAdapter(Payment.class, new TransactionSerializer());
+        Gson gson = gsonBuilder.create();
         String json = gson.toJson(transaction1);
         String comparison = "{" +
                 "\"CLASSNAME\":\"Payment\"," +
@@ -77,8 +86,8 @@ public class TransactionSerializerTest {
                 "\"amount\":1000.0," +
                 "\"description\":\"Payment\"" +
                 "}" + "}";
-        assertEquals(comparison,json);
-        Payment deserializedTransaction = gson.fromJson(json,Payment.class);
+        assertEquals(comparison, json);
+        Payment deserializedTransaction = gson.fromJson(json, Payment.class);
 
         assertEquals("26.11.2023", deserializedTransaction.getDate());
         assertEquals(1000, deserializedTransaction.getAmount());
